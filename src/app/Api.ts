a65e8e00d9d8f2d93f7baff53021e53a88a6fe84
Bruno <brunocostasilva62@gmail.com>
2024-12-07 20:54:39 -0300

    mudando a url api para produção

--- src/app/Api.ts
+++ src/app/Api.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'http://127.0.0.1:8000/api';
-/*export const API_URL = 'https://led.sitesdahora.com.br/api';*/
+/*export const API_URL = 'http://127.0.0.1:8000/api';*/
+export const API_URL = 'https://led.sitesdahora.com.br/api';
 
 
 export function GET_LEADS() {
