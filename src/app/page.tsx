b63a6ba992b22b9c4f2216af74bc53473ef1e1ed
Bruno <brunocostasilva62@gmail.com>
2024-12-12 15:49:06 -0300

    criado a pagina apenas para captrar o whatsapp do cliente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
 import "./globals.css";
-import InicioPage from "@/app/inicio/page";
-import Header from "@/componets/header";
-import SobrePage from "@/app/sobre/page";
-import BeneficiosPage from "@/app/beneficios/page";
-import ServicoesPage from "@/app/servicos/page";
-import PrecoPage from "@/app/preco/page";
-import Footer from "@/componets/footer";
-import PageTecnologia from "@/app/tecnologia/page";
 import CapturaPgae from "@/app/captura/page";
 
 
